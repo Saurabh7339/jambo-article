2bd6aa8bd4ec482f737d68ff461f7bea777bba4e
Saurabh Kumar <saurabhkr7339@gmail.com>
2021-09-09 21:04:51 +0530

    first committtt

--- articles/models.py
+++ articles/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Articles(models.Model):
 	title=models.CharField(max_length=100)
 	slug=models.SlugField()
-	body= models.TextField(max_length=100)
+	body= models.TextField(max_length=1000)
 	date=models.DateTimeField(auto_now_add=True)
 	thumb = models.ImageField(default='default.png',blank=True)
 	author = models.ForeignKey(User,on_delete=models.DO_NOTHING,default=None)
